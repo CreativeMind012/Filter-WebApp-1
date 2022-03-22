fb8c5b673c706abb0150fe3b54dd56ebd9f0c3ce
CreativeMind012 <88550031+CreativeMind012@users.noreply.github.com>
2022-03-22 18:35:16 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function preload(){
+    n = m;
+}
+function setup(){
+    canvas = createCanvas(500, 500);
+    canvas.centre;
+}
+function draw(){
+    y=u;
+}
+function save_filtered_image(){
+    d=l;
+}
